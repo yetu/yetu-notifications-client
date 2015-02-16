bb5e3902060a29388be986248ee0e20b85e36ee2
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-02-16 14:48:14 +0100

    Removed browsers form the karma conf and updated token aud

--- karma.conf.js
+++ karma.conf.js
@@ -44,6 +44,6 @@ module.exports = function (config) {
 		port: 9876,
 		colors: true,
 		autoWatch: true,
-		browsers: ['Chrome', 'Firefox', 'Safari', 'PhantomJS']
+		browsers: ['PhantomJS']
 	});
 };
