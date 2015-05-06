4ebc17aaff72eb46943087dbcd51a6096c43cf99
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-05-06 17:05:34 +0200

    Removed forceNew flag and fixed client

--- js/client.js
+++ js/client.js
@@ -2,17 +2,16 @@
 /* global Promise */
 define([
 	'socket.io-client',
-	'reqwest',
-	'jwt-decode'
-], function (io, reqwest, jwtdecoder) {
+	'reqwest'
+], function (io, reqwest) {
 
 	/** -- Probably use this to run test against local env -- */
 	var OUTBOX = 'http://localhost:8082';
 	var INBOX = 'http://localhost:8080/publish';
 
 	/** -- And this as the devlopment env --
-	var INBOX = 'https://inbox-dev.yetu.me/publish';
-	var OUTBOX = 'https://outbox-dev.yetu.me'; */
+	 var INBOX = 'https://inbox-dev.yetu.me/publish';
+	 var OUTBOX = 'https://outbox-dev.yetu.me'; */
 
 	var JOINED_DELAY = 100;
 	var connectionParams = {};
@@ -29,7 +28,7 @@ define([
 		};
 	};
 
-	function send(token) {
+	function send (token) {
 		return function (payload) {
 			payload.timeToLive = payload.timeToLive || 10000;
 
@@ -51,59 +50,52 @@ define([
 		};
 	}
 
-	function compose(f1, f2) {
+	function compose (f1, f2) {
 		return function () {
 			return f1(f2.apply(this, [].splice.call(arguments, 0)));
 		};
 	}
 
-	function string2Json(string) {
+	function string2Json (string) {
 		return JSON.parse(string);
 	}
 
-	function subscribe(token) {
+
+	function processSubscription (socket, resolve, reject) {
 		return function (payload, onData, onError) {
-			var socket = io.connect(connectionParams.outboxUrl + '/?token=' +
-			 token, {secure: true});
-			return new Promise(function(resolve, reject){
-				socket.on('connect', function () {
-
-					var clientId = payload.clientId;
-					if (typeof clientId === 'undefined' || !clientId) {
-
-						var decoded = jwtdecoder(token);
-						if (typeof decoded !== 'undefined' && decoded) {
-							clientId = decoded.clientId;
-						}
-					}
-
-					socket.emit('join', payload);
-					socket.on('joined', function(event){
-						// wait for really connecting to MQ, can be improved
-						// by sending separate message from outbox to data
-						setTimeout(function(){
-							resolve(function onDataPutter(handler){
-								socket.on('data', handler);
-							});
-						}, connectionParams.joinedDelay);
+			socket.emit('join', payload);
+			socket.on('joined', function (event) {
+				// wait for really connecting to MQ, can be improved
+				// by sending separate message from outbox to data
+				setTimeout(function () {
+					resolve(function onDataPutter (handler) {
+						socket.on(payload.clientId + '.' + payload.event, handler);
 					});
-
-					if (onData) {
-						socket.on(clientId + '.' + payload.event, compose(onData, string2Json));
-					}
-
-					if (onError) {
-						socket.on('error', onError);
-					}
-				});
-				// potentially a buggy place
-				// need to check the socket-io behavior
-				socket.on('disconnect', reject);
-
-				socket.on('error', reject);
+				}, connectionParams.joinedDelay);
 			});
 
+			if (onData) {
+				socket.on(payload.clientId + '.' + payload.event, compose(onData, string2Json));
+			}
 
+			if (onError) {
+				socket.on('error', onError);
+			}
+		};
+	}
+
+	function subscribe (token) {
+		return function (payload, onData, onError) {
+			var socket = io.connect(connectionParams.outboxUrl + '/?token=' +
+			token, {secure: true});
+			return new Promise(function (resolve, reject) {
+				if (socket.connected) {
+					processSubscription(socket, resolve, reject)(payload, onData, onError)
+				}
+				socket.on('connect',function(){
+					processSubscription(socket, resolve, reject)(payload, onData, onError)
+				});
+			});
 		};
 	}
 
